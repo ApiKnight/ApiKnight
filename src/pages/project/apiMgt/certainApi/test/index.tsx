cc5fbf2c1958ba66222a288a21de57426abf6b06
Escaay <2790751195@qq.com>
2023-07-30 10:51:44 +0800

    路由配置

---
+++ src/pages/project/apiMgt/certainApi/test/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.less'
+
+const Test: React.FunctionComponent = () => {
+    return (
+        <div>
+            Test
+        </div>
+    )
+}
+
+export default Test
---
+++ src/pages/project/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './index.less'
+import { Outlet, Link } from 'react-router-dom'
+const Project: React.FunctionComponent = () => {
+    return (
+        <>
+            <div>
+                <div>具体项目</div>
+                <ul>
+                    <li>
+                        <Link to='/project/apiMgt'>接口管理</Link>
+
+                    </li>
+                    <li>
+                        <Link to='/project/memberMgt'>成员/权限管理</Link>
+                    </li>
+                </ul>
+                <Outlet />
+            </div>
+        </>
+    )
+}
+
+export default Project

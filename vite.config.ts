dde2e09cea78c81a92c74ca6892245c9b64804a9
LuoKing <luoki1313@163.com>
2023-08-02 23:11:59 +0800

    build: 增加路径别名@

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+const resolve = (dir: string) => path.join(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			// 根路径别名
+			'@': resolve('src')
+		},
+		extensions: ['.tsx', '.ts', '.jsx', '.js', '.json']
+	}
 })

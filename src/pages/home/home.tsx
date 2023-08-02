785a1e0aca6198ee5e255e7134322adcb5af2e2b
LuoKing <luoki1313@163.com>
2023-08-02 21:08:37 +0800

    chore: wuhu

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import './index.less'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./index.less";
+import { Link } from "react-router-dom";
 
 const Home: React.FunctionComponent = () => {
-    return (
-        <div>
-            <div>主页</div>
-            <div><Link to='/project'>项目1</Link></div>
-            <div><Link to='/user'>用户中心</Link></div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>主页</div>
+      <div>
+        <Link to="/project">项目1</Link>
+      </div>
+      <div>
+        <Link to="/user">用户中心</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;

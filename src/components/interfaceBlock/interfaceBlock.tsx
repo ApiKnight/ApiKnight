85bcabc4f05dd28ea79131c87396b2c508985a42
liuyuyang <liuyy33@asiainfo.com>
2023-08-03 00:27:49 +0800

    change(components)修改了树目录部分的部分样式与功能

--- src/components/interfaceBlock/interfaceBlock.tsx
+++ src/components/interfaceBlock/interfaceBlock.tsx
@@ -3,6 +3,9 @@ import "./index.less";
 import AddBtn from './addBtn';
 import DelBtn from './delBtn';
 import { createJsErrorMonitor } from '../../../sdk/createJsErrorMonitor';
+import { createResourceErrorMonitor } from '../../../sdk/createResourceErrorMonitor';
+import { createPromiseErrorMonitor } from '../../../sdk/createPromiseErrorMonitor';
+import { createXhrMonitor } from '../../../sdk/createXhrMonitor';
 
 interface TitleNode {
     id: number;
@@ -13,12 +16,18 @@ interface Props {
     data:TitleNode;
 }
 
+function startMonitor() {
+  createJsErrorMonitor('renderTree').start();
+  createResourceErrorMonitor('renderTree').start();
+  createPromiseErrorMonitor('renderTree').start();
+  createXhrMonitor('renderTree').start();
+}
+
 const InterfaceBlock: React.FunctionComponent<{data:TitleNode}> = (props:Props) => {
-    createJsErrorMonitor('interfaceBlock').start();
+    startMonitor();
     const [show,setShowState] = useState(false)
     function changeBtnState():void {
       setShowState(!show);
-      console.log(show);
     }
     const {data} = props; 
     return (

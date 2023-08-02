8f7d5b5015f3a5df30d7790bdae201ef917522e0
LuoKing <luoki1313@163.com>
2023-08-02 23:47:06 +0800

    refactor: 重构使用公共组件重构文档模块的请求操作部分

--- src/pages/project/apiMgt/certainApi/document/c-pages/doc-operator/index.tsx
+++ src/pages/project/apiMgt/certainApi/document/c-pages/doc-operator/index.tsx
@@ -1,87 +1,36 @@
-import React, { memo, useState } from "react";
-import { Button, Input, Select, Space } from "antd";
-
-import type { ReqOptType } from "./type";
-import "./index.less";
-
-// 请求类型信息
-const methodOptions: ReqOptType[] = [
-  { label: "GET", value: "GET", colorClassName: "color-get" },
-  {
-    label: "POST",
-    value: "POST",
-    colorClassName: "color-post",
-  },
-  { label: "PUT", value: "PUT", colorClassName: "color-put" },
-  { label: "DELETE", value: "DELETE", colorClassName: "color-delete" },
-  { label: "OPTIONS", value: "OPTIONS", colorClassName: "color-options" },
-  { label: "HEAD", value: "HEAD", colorClassName: "color-head" },
-  { label: "PATCH", value: "PATCH", colorClassName: "color-patch" },
-];
+import React, { memo, useState } from 'react'
+import { Button } from 'antd'
+import type { ApiOptReqOptType } from '@/components/ApiOperator/type'
+import './index.less'
+import ApiOperator from '@/components/ApiOperator'
 
 const DocOperator: React.FunctionComponent = memo(() => {
-  const [method, setMethod] = useState(methodOptions[0]);
-  const [selectVisible, setSelectVisible] = useState(false);
-  const [docUrl, setDocUrl] = useState("");
+	const [method, setMethod] = useState<ApiOptReqOptType>()
+	const [docUrl, setDocUrl] = useState('')
 
-  // 手动渲染请求方式下拉列表
-  function getDropDownEle(): React.ReactElement {
-    return (
-      <ul className="method-select">
-        {methodOptions.map((item, index) => (
-          <li
-            className={["method-item", item.colorClassName].join(" ")}
-            onClick={() => methodChooseHandle(item)}
-            key={index}
-          >
-            {item.label}
-          </li>
-        ))}
-      </ul>
-    );
-  }
+	// 输入框改变事件
+	function urlInputChangeHandle(e: React.ChangeEvent<HTMLInputElement>): void {
+		setDocUrl(e.target.value)
+		// TODO:加上防抖与节流
+		console.log(e.target.value)
+	}
 
-  // 选择请求方式点击事件
-  function methodChooseHandle(req: ReqOptType): void {
-    console.log(req);
-    setSelectVisible(false);
-    setMethod(req);
-  }
+	return (
+		<div className="doc-operator">
+			<ApiOperator
+				methodValue={method}
+				onOptionChange={(m) => setMethod(m)}
+				onInputChange={(e) => urlInputChangeHandle(e)}
+				inputValue={docUrl}
+			>
+				<Button className="btn" type="primary">
+					保存
+				</Button>
+				<Button className="btn">运行</Button>
+				<Button className="btn">删除</Button>
+			</ApiOperator>
+		</div>
+	)
+})
 
-  // 输入框改变事件
-  function urlInputChangeHandle(e: React.ChangeEvent<HTMLInputElement>): void {
-    setDocUrl(e.target.value);
-    // TODO:加上防抖与节流
-  }
-
-  return (
-    <div className="doc-operator">
-      <div className={["left-info", method.colorClassName].join(" ")}>
-        <Space.Compact block>
-          <Select
-            open={selectVisible}
-            onDropdownVisibleChange={(visible) => setSelectVisible(visible)}
-            // options={methodOptions}
-            optionLabelProp="value"
-            defaultValue={method}
-            value={method}
-            options={methodOptions}
-            popupMatchSelectWidth={100}
-            dropdownRender={getDropDownEle}
-            onSelect={(e) => console.log(e)}
-          />
-          <Input value={docUrl} onChange={(e) => urlInputChangeHandle(e)} />
-        </Space.Compact>
-      </div>
-      <div className="right-warp">
-        <Button className="btn" type="primary">
-          保存
-        </Button>
-        <Button className="btn">运行</Button>
-        <Button className="btn">删除</Button>
-      </div>
-    </div>
-  );
-});
-
-export default DocOperator;
+export default DocOperator
--- src/pages/project/apiMgt/certainApi/document/index.tsx
+++ src/pages/project/apiMgt/certainApi/document/index.tsx
@@ -1,19 +1,20 @@
-import React, { memo } from "react";
-import "./index.less";
-import DocOperator from "./c-pages/doc-operator";
-import DocInfo from "./c-pages/doc-info";
-import DocRequest from "./c-pages/doc-request";
-import DocResponse from "./c-pages/doc-response";
+import React, { memo } from 'react'
+import { Button } from 'antd'
+import './index.less'
+import DocOperator from './c-pages/doc-operator'
+import DocInfo from './c-pages/doc-info'
+import DocRequest from './c-pages/doc-request'
+import DocResponse from './c-pages/doc-response'
 
 const Document: React.FunctionComponent = memo(() => {
-  return (
-    <div className="document-page">
-      <DocOperator />
-      <DocInfo />
-      <DocRequest />
-      <DocResponse />
-    </div>
-  );
-});
+	return (
+		<div className="document-page">
+			<DocOperator />
+			<DocInfo />
+			<DocRequest />
+			<DocResponse />
+		</div>
+	)
+})
 
-export default Document;
+export default Document

081fd7f17485db8730437353c9b03528381df906
Escaay <2790751195@qq.com>
2023-08-05 17:01:12 +0800

    迁移接口目录到certainApi

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,35 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './index.less';
 import { Link } from 'react-router-dom';
 import { Counter } from '../../components/Counter.tsx';
-import { useSelector, useDispatch } from 'react-redux';
-import InterfaceBlock from '@/components/interfaceBlock/interfaceBlock.tsx';
-import RenderTree from '@/components/renderTree/renderTree.tsx';
-import { ArrayItem } from '@/utils/arrayToTree.ts';
-import { assign } from '@/store/modules/dirArraySlice.ts';
-import { RootState } from '@/store/index.ts';
-
-interface MakeValue {
-  value: ArrayItem[];
-}
-
-const data = [
-  { key: 1, title: <InterfaceBlock data={{ id: 1, title: "接口目录1", pid: 0 }} />, type: "file", pid: 0 },
-  { key: 2, title: <InterfaceBlock data={{ id: 2, title: "接口目录2", pid: 1 }} />, type: "file", pid: 1 },
-];
 
 const Home: React.FunctionComponent = () => {
-  const [makeValue, setMakeValue] = useState<MakeValue>({ value: data });
-  const dispatch = useDispatch();
-  const dirArray = useSelector((state: RootState) => state.dirArray.value);
-
-  useEffect(() => {
-    dispatch(assign(data));
-  }, []);
-
-  useEffect(() => {
-    setMakeValue({ value: dirArray });
-  }, [dirArray]);
-
+  
   return (
     <div>
       <div>主页</div>
@@ -40,10 +14,7 @@ const Home: React.FunctionComponent = () => {
       <div>
         <Link to='/user'>用户中心</Link>
       </div>
-      <div>
         <Counter />
-      </div>
-      <RenderTree data={makeValue.value}></RenderTree>
     </div>
   );
 };

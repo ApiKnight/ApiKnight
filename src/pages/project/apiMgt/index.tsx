c0a76ef308d35fbfdb1af76f72d4f86c83ba28a2
@qiuwenjing <qiuwenjing@rt-thread.com>
2023-08-11 15:30:23 +0800

    合并

--- src/pages/project/apiMgt/index.tsx
+++ src/pages/project/apiMgt/index.tsx
@@ -6,6 +6,68 @@ import { ArrayItem } from '@/types/arrayToTree'
 import { FlatItem, FlatItemValue } from '@/types/mergeFlatArrays'
 import { mergeFlatArrays } from '@/utils/mergeFlatArrays'
 const ApiMgt: React.FunctionComponent = () => {
+  const a1: FlatItem[] = [
+    {
+      id: '1',
+      name: '根目录',
+      project_id: 100,
+      parent_id: null,
+    },
+    {
+      id: '2',
+      name: '接口目录1',
+      project_id: 100,
+      parent_id: '1',
+    },
+    {
+      id: '3',
+      name: '接口目录1',
+      project_id: 100,
+      parent_id: '1',
+    },
+  ]
+  const a2: FlatItem[] = [
+    {
+      id: '4',
+      name: 'getList',
+      project_id: 100,
+      parent_id: '3',
+    },
+  ]
+  const dd: ArrayItem[] = mergeFlatArrays(a1, a2, 100)
+  const cs: FlatItemValue = {
+    value_1: a1,
+    value_2: a2,
+    target: 100,
+  }
+  const d: ArrayItem[] = [
+    {
+      key: '1',
+      title: { key: '1', title: '根目录', pid: null, type: 'FILE' },
+      type: 'FILE',
+      pid: null,
+    },
+    {
+      key: '2',
+      title: { key: '2', title: '接口目录1', pid: '1', type: 'FILE' },
+      type: 'FILE',
+      pid: '1',
+    },
+    {
+      key: '3',
+      title: { key: '3', title: '接口目录2', pid: '1', type: 'FILE' },
+      type: 'FILE',
+      pid: '1',
+    },
+    {
+      key: '4',
+      title: { key: '4', title: 'getList', pid: '3', type: 'GET' },
+      type: 'GET',
+      pid: '3',
+    },
+  ]
+  console.log(dd)
+  console.log(d)
   return (
     <>
       <div>ApiMgt</div>
@@ -20,6 +82,7 @@ const ApiMgt: React.FunctionComponent = () => {
       <div>
         <Outlet />
       </div>
+      {/* <RenderTree data={dd} /> */}
     </>
   )
 }

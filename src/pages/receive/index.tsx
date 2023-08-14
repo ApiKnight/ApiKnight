d76a2b85410f8dfd0cb4c279c834bf86f0d789c0
liuyuyang <2281487673@qq.com>
2023-08-14 10:18:46 +0800

    ‘“

--- src/pages/receive/index.tsx
+++ src/pages/receive/index.tsx
@@ -1,23 +1,44 @@
-import React from "react";
-import {Button} from "antd";
+import React, {useEffect, useState} from "react";
+import { Button, Input, message, notification } from 'antd'
 import "./index.less"
-import {useSearchParams} from "react-router-dom";
+import {useNavigate, useSearchParams} from "react-router-dom";
 import request from "@/api/request";
+import type { NotificationPlacement } from 'antd/es/notification/interface'
 
 const Receive:React.FunctionComponent = () => {
+    const navigate = useNavigate();
     let [searchParams, setSearchParams] = useSearchParams();
+    const [api, contextHolder] = notification.useNotification()
     function sendJoin() {
-        request.post("/v1/invite/receive",{projectid:searchParams.get('projectid')},{}).then((resp)=>{
-            console.log("申请成功!!!!");
+        request.post("/v1/invite/receive",{projectid:Number(searchParams.get('projectid'))},{}).then((resp)=>{
+            console.log(resp.data.message)
+            const openNotification = (placement: NotificationPlacement) => {
+                api.info({
+                    message: <p>{resp.data.message}</p>,
+                    description: <p>3s后返回主界面</p>,
+                    placement,
+                })
+            }
+            openNotification('topLeft')
+            setTimeout(()=>{
+                navigate('/')
+            },3000)
         })
     }
+    const [projectName,setProjectName] = useState('')
+    useEffect(()=>{
+        request.post(`/v1/project/query`,{projectid:Number(searchParams.get('projectid'))},{}).then((resp:any)=>{
+            setProjectName(resp.data.data.projectname as string)
+        })
+    },[])
     return (
         <div className="receive">
+            {contextHolder}
             <h1>ApiKnight</h1>
             <p>
-                刘宇洋 在 ApiKnight 中邀请您加入 {searchParams.get('projectid')}
+                刘宇洋 在 ApiKnight 中邀请您加入 <strong>{projectName}</strong> 项目
             </p>
-            <div>
+            <div className="receive-btn">
                 <Button type="primary" size="large" onClick={sendJoin}>立即加入</Button>
             </div>
             <p>

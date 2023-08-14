4810a368d7adab04c7353fe89021095e9da862ef
liuyuyang <2281487673@qq.com>
2023-08-14 10:39:08 +0800

    完善了projectid相关部分功能

--- src/components/Invite/link.tsx
+++ src/components/Invite/link.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Button, Input, notification } from 'antd'
 import './link.less'
 import type { NotificationPlacement } from 'antd/es/notification/interface'
+import {useSelector} from "react-redux";
+import {RootState} from "../../store";
 
 const Link: React.FunctionComponent = () => {
-  // 示例链接
-  const link = `http://example.com/xxx?token=${localStorage.getItem('token')}`
   const [api, contextHolder] = notification.useNotification()
-
+  const projectId = useSelector((state: RootState) => state.project.project_id)
+  // 示例链接
+  const link = `http://127.0.0.1:5173/receive?projectid=${Number(projectId)}`
   const openNotification = (placement: NotificationPlacement) => {
     api.info({
       message: <p>复制成功</p>,

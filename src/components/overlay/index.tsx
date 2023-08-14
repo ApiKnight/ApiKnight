94f02b49f9f956aed18de5dd4cd2f463d07a6c3b
liuyuyang <2281487673@qq.com>
2023-08-14 11:41:04 +0800

    style为invite组件添加了遮罩层

---
+++ src/components/overlay/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.less"
+
+const Overlay:React.FunctionComponent = () => {
+    return ReactDOM.createPortal(
+        <div className="Overlay">
+
+        </div>
+        ,document.body
+    )
+}
+
+export default Overlay;

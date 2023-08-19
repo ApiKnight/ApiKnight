f3bc4fc5fe1e82210faf0d2e599b9030ef865b86
LuoKing <luoki1313@163.com>
2023-08-20 05:53:30 +0800

    feat: 创建全局可修改的antdesign主题

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,21 @@ import './assets/css/index.less'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './store/index.ts'
 import { Provider } from 'react-redux'
+import { ConfigProvider, ThemeConfig } from 'antd'
+
+// 定制主题
+const theme: ThemeConfig = {
+  token: {
+    colorPrimary: '#1677ff',
+  },
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <ConfigProvider theme={theme}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ConfigProvider>,
 )

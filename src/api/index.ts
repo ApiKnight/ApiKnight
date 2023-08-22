d48e8f1618ee208be41180b3781718e65182d0e1
LuoKing <luoki1313@163.com>
2023-08-23 00:47:25 +0800

    feat: 增加mock模块

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,9 @@
+// 导出类型
 export * from './apis/type'
+export * from './mock/type'
+export * from './user/type'
+
+// 导出功能
 export * from './apis/index'
+export * from './mock/index'
+export * from './user/index'

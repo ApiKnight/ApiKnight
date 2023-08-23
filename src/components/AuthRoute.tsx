19f2388dd65c90e66b23059ef0d1182c8c30ffd0
liuyuyang <2281487673@qq.com>
2023-08-23 11:16:08 +0800

    sty;e(Loading)修改了Loading样式

--- src/components/AuthRoute.tsx
+++ src/components/AuthRoute.tsx
@@ -1,13 +1,21 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '@/utils/useAuth'
-
+import { Spin } from 'antd'
 import { ReactNode, useState, useEffect, useLayoutEffect } from 'react'
+import ReactDOM from 'react-dom';
+import "./AuthRoute.less"
 
 interface AuthRouteProps {
   children: ReactNode
 }
 const AuthRoute = ({ children }: AuthRouteProps) => {
-  const [isLogin, setIsLogin] = useState<any>(<div>Loading</div>)
+  const [isLogin, setIsLogin] = useState<any>(
+        <div className='Spin-Style'>
+          <Spin tip='Loading' size='large'>
+                <div className='content' />
+          </Spin>
+        </div>
+  )
   useEffect(() => {
     useAuth().then((result) => {
       console.log('result',result);

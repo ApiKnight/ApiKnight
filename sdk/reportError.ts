7b59d2d308e859c84a0dcfcf36cfe0b67632f333
liuyuyang <2281487673@qq.com>
2023-10-11 12:47:21 +0800

    change(sdk)更改埋点上报的方式

--- sdk/reportError.ts
+++ sdk/reportError.ts
@@ -10,5 +10,5 @@ export function reportError(error: any, url: string, type: string) {
     type: type,
     message: JSON.stringify(error),
   }
-  request.post('/v1/monitor/upload', sendData, {}).then((resp) => {})
+  navigator.sendBeacon('http://polaris.lyyfsq.club:7002/api/v1/monitor/upload',JSON.stringify(sendData))
 }

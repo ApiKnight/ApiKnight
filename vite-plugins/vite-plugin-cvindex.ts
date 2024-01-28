acf30eff1f5dcb19a8e1bf40a104ccad62cb5e36
liuyuyang <2281487673@qq.com>
2024-01-29 01:09:31 +0800

    styled: format some code

--- vite-plugins/vite-plugin-cvindex.ts
+++ vite-plugins/vite-plugin-cvindex.ts
@@ -7,20 +7,13 @@ export default function cvIndex(): Plugin {
     name: 'vite:cvIndex',
 
     writeBundle() {
-      // This hook is called at the beginning of the build process
       const outputDir = './dist'
       const indexPath = path.join(outputDir, 'index.html')
-      console.log('\n', indexPath)
-
-      // Check if index.html exists
       if (fs.existsSync(indexPath)) {
         const targetPath = path.join(outputDir, '404.html')
         const indexContent = fs.readFileSync(indexPath, 'utf-8')
-
-        // Write index.html content to 404.html
         fs.writeFileSync(targetPath, indexContent, 'utf-8')
-
-        console.log('Successfully created 404.html')
+        console.log('\nSuccessfully created 404.html')
       } else {
         console.log('index.html does not exist. Skipping 404.html creation.')
       }

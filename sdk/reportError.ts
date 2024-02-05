48c20b799685cc8dadfa49afbab40644538529d7
Yuyang Liu <yuyang.liu@rightcapital.com>
2024-02-05 18:07:48 +0800

    feat: enhance FrontEnd monitor SDK

--- sdk/reportError.ts
+++ sdk/reportError.ts
@@ -3,6 +3,8 @@ export function reportError(error: any, url: string, type: string) {
   if (url === '' || url === undefined) {
     url = window.location.pathname
   }
+  error.userId = localStorage.getItem('user_id') ? localStorage.getItem('user_id') : "New User"
+  error.curTime = +new Date();
   const sendData = {
     url: url,
     type: type,
